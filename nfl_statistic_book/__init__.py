abf6a4c85dd02deae6a6a8b7910f179c9e8fa9d4
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-13 15:36:07 -0400

    Fixing some package issues

--- nfl_statistic_book/__init__.py
+++ nfl_statistic_book/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for nfl-statistic-book."""
+from nfl_statistic_book import nfl_statistic_book
+from team import Team
 
 __author__ = """Kaleb Swartz"""
 __email__ = 'kalebswartz7@gmail.com'
