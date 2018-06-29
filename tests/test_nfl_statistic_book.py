283c916b76dbc20bd91bd2958285306c0035cb5b
Kaleb Swartz <kalebswartz7@gmail.com>
2018-06-29 16:20:19 -0400

    Updated test file

--- tests/test_nfl_statistic_book.py
+++ tests/test_nfl_statistic_book.py
@@ -2,13 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `nfl_statistic_book` package."""
-
-
 import unittest
-from click.testing import CliRunner
 
 from nfl_statistic_book import nfl_statistic_book
-from nfl_statistic_book import cli
 
 
 class TestNfl_statistic_book(unittest.TestCase):
@@ -23,12 +19,3 @@ class TestNfl_statistic_book(unittest.TestCase):
     def test_000_something(self):
         """Test something."""
 
-    def test_command_line_interface(self):
-        """Test the CLI."""
-        runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'nfl_statistic_book.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
