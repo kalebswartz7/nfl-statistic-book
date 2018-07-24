a8b8bd27f0727674dfd0614ee96a29c4570cb495
Kaleb Swartz <kalebswartz7@gmail.com>
2018-07-24 09:28:52 -0400

    Small change to init

--- nfl_statistic_book/__init__.py
+++ nfl_statistic_book/__init__.py
@@ -2,8 +2,6 @@
 
 """Top-level package for nfl-statistic-book."""
 
-from nfl_statistic_book import team
-
 __author__ = """Kaleb Swartz"""
 __email__ = 'kalebswartz7@gmail.com'
 __version__ = '1.0.0'
